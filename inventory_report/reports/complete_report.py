efe02826e1b0a8c6e1ebaa26f99e342ba1b9613d
RichardPMachado <richardmachado2211@gmail.com>
2023-05-09 02:49:08 -0300

    feat[requisito 3]: Complete Report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,30 @@
-class CompleteReport:
-    pass
+from typing import List, Dict
+from .simple_report import SimpleReport
+# import datetime
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(prods: List[Dict]) -> str:
+        simple_report_result = SimpleReport.generate(prods)
+        companies = dict()
+        for produto in prods:
+            if produto["nome_da_empresa"] in companies:
+                companies[produto["nome_da_empresa"]] += 1
+            else:
+                companies[produto["nome_da_empresa"]] = 1
+
+        companies_list = list(companies.items())
+
+        str_products_stock = ''
+
+        for e in companies_list:
+            str_products_stock += f"- {e[0]}: {e[1]}\n"
+
+        # print(f"{str_products_stock}")
+
+        return (
+            f"{simple_report_result}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{str_products_stock}"
+            )
