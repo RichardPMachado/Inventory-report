b051e1492977ba5cd863e3b0b6813fa5547f32dd
RichardPMachado <richardmachado2211@gmail.com>
2023-05-09 05:26:25 -0300

    feat[requisito 4]: ler typo de arquivo e retornar complete report ou simple report

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,28 @@
+import os
 import pandas as pd
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
     @staticmethod
     def import_data(path: str, report: str):
-        csv_read = Inventory.csv_report(path)
-        if (report == "completo"):
-            complet_simple_report = CompleteReport.generate(csv_read)
+        type_report = Inventory.verify_path(path)
+        print('ooooo', type_report)
+
+        if (type_report == ".csv"):
+            report_read = Inventory.csv_report(path),
+        elif (type_report == ".json"):
+            report_read = Inventory.json_report(path),
         else:
-            complet_simple_report = SimpleReport.generate(csv_read)
+            report_read = Inventory.xml_report(path)
+
+        # print(report_read)
+        if (report == "completo"):
+            complet_simple_report = CompleteReport.generate(report_read)
+        else:
+            complet_simple_report = SimpleReport.generate(report_read)
         return complet_simple_report
 
     @classmethod
@@ -18,3 +30,27 @@ class Inventory:
         csv_read = pd.read_csv(path)
         lista_de_dict = csv_read.to_dict('records')
         return lista_de_dict
+
+    @classmethod
+    def json_report(cls, path):
+        json_read = pd.read_json(path)
+        lista_de_dict = json_read.to_dict('records')
+        return lista_de_dict
+
+    @classmethod
+    def xml_report(cls, path):
+        tree = ET.parse(path)
+        root = tree.getroot()
+        data = []
+
+        for element in root:
+            prod = {}
+            for child in element:
+                prod[child.tag] = child.text
+            data.append(prod)
+        return data
+
+    @classmethod
+    def verify_path(cls, path):
+        type_report = os.path.splitext(path)
+        return type_report
