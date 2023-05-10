85f8263abbe493711c28829010d614cdd139180b
RichardPMachado <richardmachado2211@gmail.com>
2023-05-09 21:25:25 -0300

    fix[requisito 7]: importers

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -8,11 +8,8 @@ class CsvImporter(Importer):
 
     @classmethod
     def import_data(cls, path):
-        try:
-            if not path.endswith('.csv'):
-                raise ValueError("Arquivo inválido")
-            csv_read = pd.read_csv(path)
-            dict_list = csv_read.to_dict('records')
-            return dict_list
-        except ValueError:
-            raise ValueError
+        if not path.endswith('.csv'):
+            raise ValueError("Arquivo inválido")
+        csv_read = pd.read_csv(path)
+        dict_list = csv_read.to_dict('records')
+        return dict_list
--- inventory_report/importer/json_importer.py
+++ inventory_report/importer/json_importer.py
@@ -9,12 +9,9 @@ class JsonImporter(Importer):
 
     @classmethod
     def import_data(cls, path):
-        try:
-            type_report = os.path.splitext(path)[1]
-            if not type_report.endswith('.json'):
-                raise ValueError("Arquivo inválido")
-            json_read = pd.read_json(path)
-            dict_list = json_read.to_dict('records')
-            return dict_list
-        except ValueError:
-            raise ValueError
+        type_report = os.path.splitext(path)[1]
+        if not type_report.endswith('.json'):
+            raise ValueError("Arquivo inválido")
+        json_read = pd.read_json(path)
+        dict_list = json_read.to_dict('records')
+        return dict_list
